38abf4f002170ac3190232d61ec0e9fb328ff845
marcow <marcogithub@163.com>
2022-10-04 08:20:08 +0800

    [DEBUG] element frame, logHints
    Signed-off-by: marcow <marcogithub@163.com>

--- ViMac-Swift/Modes/HintsViewController.swift
+++ ViMac-Swift/Modes/HintsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import os
 import AXSwift
 
 class HintsViewController: NSViewController {
@@ -36,6 +37,7 @@ class HintsViewController: NSViewController {
         self.typed = typed
         self.modifiers = modifiers
         super.init(nibName: nil, bundle: nil)
+        view.wantsLayer = true
     }
     
     required init?(coder: NSCoder) {
@@ -52,10 +54,20 @@ class HintsViewController: NSViewController {
         self.hintViews = hints
             .map { renderHint($0, modifiers: modifiers) }
             .compactMap({ $0 })
-
+        #if DEBUG
+        logHints()
+        view.layer?.sublayers = []
+        for hintView in self.hintViews {
+            if let shape = shape(with: hintView) {
+                view.layer?.addSublayer(shape)
+            }
+            view.addSubview(hintView)
+        }
+        #else
         for hintView in self.hintViews {
             self.view.addSubview(hintView)
         }
+        #endif
         
         helpView.isHidden = true
     }
@@ -137,6 +149,48 @@ class HintsViewController: NSViewController {
     
     @objc func helpOff() {
         helpView.isHidden = true
+    }
+    
+    func logHints() {
+        var roleCounts = [String : Int]()
+        hints.forEach {
+            let role = $0.element.role
+            let key = String(format: "%-2s %@", role.roleKey.cstr!, role)
+            if let count = roleCounts[key] {
+                roleCounts[key] = count + 1
+            } else {
+                roleCounts[key] = 1
+            }
+        }
+        let info = roleCounts
+            .sorted { $0.1 > $1.1 }
+            .map { String(format: "%-3d %@", $0.1, $0.0) }
+        os_log("roleCounts: %@", info)
+    }
+    
+    func shape(with hintView: HintView) -> CAShapeLayer? {
+        let shape = CAShapeLayer()
+        guard let frame = elementFrame(hintView.associatedElement) else {
+            os_log("frame is nil of element: %@", hintView.associatedElement.description)
+            return nil
+        }
+        shape.frame = frame
+        let bounds = shape.bounds
+        let path = CGMutablePath()
+        path.addLines(between: [
+            .zero,
+            CGPoint(x: bounds.origin.x, y: bounds.maxY),
+            CGPoint(x: bounds.maxX, y: bounds.maxY),
+            CGPoint(x: bounds.maxX, y: bounds.origin.y),
+            .zero
+        ])
+        shape.path = path
+        shape.lineWidth = 0.8
+        //            shape.lineDashPattern = [2, 4]
+        shape.strokeColor = NSColor.red.cgColor
+        shape.backgroundColor = nil
+        shape.fillColor = nil
+        return shape
     }
     
     func switchShowHelp(with info: String, forceOn: Bool = false) {
